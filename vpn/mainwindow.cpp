9e7269ef6c007158b9a6d0ad5c568cb29aaabba3
Mark Duppenthaler <mduppes@gmail.com>
2012-10-10 21:27:41 +0000

    Added checkbox, tcp connection sockets.. will update later.

--- vpn/mainwindow.cpp
+++ vpn/mainwindow.cpp
@@ -1,14 +1,32 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    serverMode = false;
+    connected = false;
+    listening = false;
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_checkBox_server_stateChanged(int checked)
+{
+    if(checked) {
+        serverMode = true;
+        qDebug() << "In Server Mode";
+        ui->pushButton_connect->setText("Listen");
+    } else {
+        serverMode = false;
+        qDebug() << "In Client Mode";
+        ui->pushButton_connect->setText("Connect");
+    }
 }
--- vpn/mainwindow.h
+++ vpn/mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QString>
 
 namespace Ui {
 class MainWindow;
@@ -14,9 +17,18 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-    
+
+private slots:
+    void on_checkBox_server_stateChanged(int checked);
+
 private:
     Ui::MainWindow *ui;
+    QTcpServer* serverSocket;
+    QTcpSocket* clientSocket;
+    QString logString;
+    bool serverMode;
+    bool connected;
+    bool listening;
 };
 
 #endif // MAINWINDOW_H
