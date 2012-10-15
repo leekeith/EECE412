8a447240070861ccd0985c209dd40e238e38ac32
Keith Lee <keith.n.lee@gmail.com>
2012-10-14 20:58:59 -0700

    Supposed to add feistel and DH to encryptionhelper.

--- vpn/encryptionhelper.cpp
+++ vpn/encryptionhelper.cpp
@@ -2,28 +2,172 @@
 #include <cstdlib>
 #include <ctime>
 #include <cmath>
+#include<limits>
 
-const int MAX_SECRET_INTEGER = 30;
+const unsigned int MAX_SECRET_INTEGER = 8;
 
-EncryptionHelper::EncryptionHelper(QObject *parent, Logging* log) :
-    QObject(parent)
+EncryptionHelper::EncryptionHelper()//QObject *parent, Logging* log) :
+    //QObject(parent)
 {
-    this->log = log;
+    //this->log = log;
 
     // Initialize Diffie Helman key exchange constants
-    this->dhGenerator = 5;
-    this->dhPrime = 23;
+    this->dhGenerator = 1;
+    this->dhPrime = 3571;
+	this->p_DH_Key=0;
+	this->DH_Key=0;
     // Initialize random number generator seed
     std::srand(std::time(0));
 }
 
-
-unsigned long EncryptionHelper::generatePartialDiffieHelmanKey() {
+unsigned long EncryptionHelper::get_p_DH_key()
+{
+	return this->p_DH_Key;
+}
+void EncryptionHelper::gen_p_DH_key() {
 
     // generate secret integer a
-    secretInteger = std::rand() % MAX_SECRET_INTEGER;
+    this->secretInteger = (std::rand() % MAX_SECRET_INTEGER)+1;
 
     // Calculate g^a mod p, and store it for calculating the session key later.
-    partialDiffieHelmanKey = ((unsigned long) std::pow((double)dhGenerator, secretInteger)) % dhPrime;
-    return partialDiffieHelmanKey;
+    this->p_DH_Key=((unsigned long)(std::pow((long double)this->dhGenerator, this->secretInteger)))%this->dhPrime;
+	
 }
+
+void EncryptionHelper::config_DH(std::string password)
+{
+	for(int i=0;i<password.size();i++)
+	{
+		this->dhGenerator+=(password[i]+password[(3*i)%(password.size()-1)])/(i+1);
+	}
+	this->gen_p_DH_key();
+}
+
+void EncryptionHelper::gen_DH_key(unsigned long p_DH_key_recd)
+{
+	unsigned long otherNum;
+	otherNum=(log(p_DH_key_recd)/log(dhGenerator));
+	this->DH_Key=((unsigned long)(std::pow((long double)this->dhGenerator, this->secretInteger*otherNum)))%this->dhPrime;
+}
+
+void EncryptionHelper::config_feis()
+{
+	int i,j;
+	for(i=0;i<NUM_ITT;i++)
+	{
+		for(j=0;j<32;j++)
+			this->keyset[i][j]=((unsigned long)(std::pow((long double)(2*this->DH_Key%(64+i)),((j+1)%8)+1)+this->DH_Key+j))%UCHAR_MAX;
+	}
+}
+
+char EncryptionHelper::feis_func(char in, int itt, int index)
+{
+	return (in*this->keyset[itt][index%32]%UCHAR_MAX);
+}
+
+void EncryptionHelper::feis_encode(char* text,char* otext,int size)
+{
+	char* L[NUM_ITT];
+	char* R[NUM_ITT];
+	int numchars;
+	
+	
+	numchars=size;
+	
+
+
+	int i,j;
+	for(i=0;i<NUM_ITT;i++)
+	{
+		L[i]=(char*)malloc(numchars/2);
+		R[i]=(char*)malloc(numchars/2);
+	}
+
+	for(i=0;i<numchars/2;i++)
+	{
+		L[0][i]=text[i];
+		R[0][i]=text[numchars/2+i];
+	}
+	
+	
+	for(i=1;i<NUM_ITT;i++)
+	{
+		for(j=0;j<numchars/2;j++)
+		{
+			L[i][j]=R[i-1][j];
+			R[i][j]=L[i-1][j]^this->feis_func(R[i-1][j],i,j);
+		}
+	}
+
+	for(i=0;i<numchars/2;i++)
+	{
+		otext[i]=L[NUM_ITT-1][i];
+		otext[numchars/2+i]=R[NUM_ITT-1][i];
+	}
+
+	otext[size-1]=0;
+
+}
+
+void EncryptionHelper::feis_decode(char* text,char* otext,int size)
+{
+	char* retval;
+	char* L[NUM_ITT];
+	char* R[NUM_ITT];
+	int numchars;
+	numchars=size;
+	
+
+
+	int i,j;
+	for(i=0;i<NUM_ITT;i++)
+	{
+		L[i]=(char*)malloc(numchars/2);
+		R[i]=(char*)malloc(numchars/2);
+	}
+
+	for(i=0;i<numchars/2;i++)
+	{
+		L[0][i]=text[i];
+		R[0][i]=text[numchars/2+i];
+	}
+	
+	for(i=1;i<NUM_ITT;i++)
+	{
+		for(j=0;j<numchars/2;j++)
+		{
+			R[i][j]=L[i-1][j];
+			L[i][j]=R[i-1][j]^feis_func(R[i][j],NUM_ITT-i,j);
+		}
+	}
+
+	for(i=0;i<numchars/2;i++)
+	{
+		otext[i]=L[NUM_ITT-1][i];
+		otext[numchars/2+i]=R[NUM_ITT-1][i];
+	}
+
+	otext[size-1]=0;
+
+}
+
+unsigned int EncryptionHelper::gen_nonce()
+{
+	unsigned int new_nonce;
+	new_nonce=rand()%UINT_MAX;
+	if(nonce.size()==0)nonce.push_back(new_nonce);
+	else
+	{
+		for(int i=0;i<nonce.size();i++)
+		{
+			if(nonce[i]==new_nonce)
+			{
+				new_nonce=rand()%UINT_MAX;
+				i=0;
+			}
+		}
+		nonce.push_back(new_nonce);
+	}
+	return new_nonce;
+}
+
--- vpn/encryptionhelper.h
+++ vpn/encryptionhelper.h
@@ -1,35 +1,51 @@
 #ifndef ENCRYPTIONHELPER_H
 #define ENCRYPTIONHELPER_H
 
-#include <QObject>
+//#include <QObject>
 #include <random>
-#include "logging.h"
+//#include "logging.h"
+#include<string>
+#define NUM_ITT 12
+#include<vector>
 
 // Class that contains encryption functions and state
 
-class EncryptionHelper : public QObject
+class EncryptionHelper //: public QObject
 {
-    Q_OBJECT
+    //Q_OBJECT
 public:
-    explicit EncryptionHelper(QObject *parent = 0, Logging* log = 0);
+    explicit EncryptionHelper();//QObject *parent = 0, Logging* log = 0);
     
     // Put in encryption functions here
-    unsigned long generatePartialDiffieHelmanKey();
+    
+	void config_DH(std::string password);
+	void gen_DH_key(unsigned long p_DH_key_recd);
+	void config_feis();
+	unsigned long get_p_DH_key();
+	void feis_encode(char* text, char* otext, int size);
+	void feis_decode(char* text, char* otext, int size);
+	unsigned int gen_nonce();
 
 
+//signals:
 
-signals:
-
-public slots:
+//public slots:
     
 private:
-    Logging * log;
+    //Logging * log;
 
     // Diffie Helman Key Exchange Constants
     int dhGenerator; // g
     int dhPrime; // p
     int secretInteger; // the random secret integer this client chooses
-    unsigned long partialDiffieHelmanKey;
+    unsigned long p_DH_Key;
+	unsigned long DH_Key;
+	unsigned char keyset[NUM_ITT][32]; 
+	std::vector<unsigned int> nonce;
+
+	char feis_func(char in, int itt, int index);
+	void gen_p_DH_key();
+
 };
 
 #endif // ENCRYPTIONHELPER_H
